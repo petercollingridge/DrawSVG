fedef3672d23470320ac001a2c654618887459f2
Peter Collingridge <peter.collingridge@gmail.com>
2011-06-19 21:53:58 +0100

    Added SVG element

--- PySVG.py
+++ PySVG.py
@@ -9,24 +9,49 @@ class SVG_Element:
             self.attributes = attributes
         else:
             self.attributes = {}
-            
+        
         if child:
             self.children = [child]
         else:
             self.children = []
-        
+    
+    def addChildElement(self, type, attributes=None, child=None):
+        self.children.append(SVG_Element(type, attributes, child))    
+    
     def output(self):
-        s = '<%s' % (self.type)
+        svg_string = '<%s' % (self.type)
         
         for key, value in self.attributes.items():
-            s += ' %s="%s"' % (key, value)
+            svg_string += ' %s="%s"' % (key, value)
         
-        if self.children:
-            s += '>\n'
-            for child in self.children:
-                s += '%s\n' % (child)
-            s += '</%s>' % (self.type)
+        if not self.children:
+            svg_string += '/>'
         else:
-            s += '/>'
+            svg_string += '>\n'
+            
+            for child in self.children:
+                if isinstance(child, SVG_Element):
+                    svg_string += child.output()
+                else:
+                    svg_string += '%s\n' % child
+                
+            svg_string += '</%s>\n' % (self.type)
+            
         
-        return s
+        return svg_string
+
+class SVG(SVG_Element):
+    """ SVG element, output includes XML document string. """
+    
+    def __init__(self, attributes=None):
+        SVG_Element.__init__(self, 'svg', attributes)
+        self.attributes['version'] = 1.1
+        self.attributes['xmlns'] = 'http://www.w3.org/2000/svg'
+    
+    def output(self):
+        svg_string  = '<?xml version="1.0" standalone="no"?>\n'
+        svg_string += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+        
+        svg_string +=  SVG_Element.output(self)
+            
+        return svg_string
