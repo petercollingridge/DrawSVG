70a978ed0fd48c854feee3aefb32a68c669bbd90
Peter Collingridge <peter.collingridge@gmail.com>
2011-06-19 22:47:04 +0100

    Added CCS styling

--- PySVG.py
+++ PySVG.py
@@ -1,9 +1,11 @@
 #   ***To do**
-#   Styles
+#   Make style an SVG_Element
+#   Allow multiple style elements with links to be added
 #   Scripts
 #   Prefered order for output
 #   Nesting parameter
 #   Automatic id generation
+#   Get element by id
 
 class SVG_Element:
     """ Generic element with attributes and potential child elements.
@@ -51,10 +53,20 @@ class SVG(SVG_Element):
     """ SVG element, output includes XML document string. """
     
     def __init__(self, attributes=None):
-        SVG_Element.__init__(self, 'svg', attributes)
+        SVG_Element.__init__(self, 'svg', attributes, SVG_Style_Element())
         self.attributes['version'] = 1.1
         self.attributes['xmlns'] = 'http://www.w3.org/2000/svg'
         self.attributes['xmlns:xlink'] = 'http://www.w3.org/1999/xlink'
+
+    def addStyle(self, element, *args):
+        """ Add style to self.style dictionary
+            in the form addStyle(element (parameter1, value1), (parameter2, value2)) """
+   
+        if element not in self.children[0].styles:
+            self.children[0].styles[element] = {}
+
+        for (key, value) in args:
+            self.children[0].styles[element][key] = value
     
     def output(self):
         svg_string  = '<?xml version="1.0"  encoding="UTF-8" standalone="no"?>\n'
@@ -63,3 +75,24 @@ class SVG(SVG_Element):
         svg_string +=  SVG_Element.output(self)
             
         return svg_string
+    
+class SVG_Style_Element(SVG_Element):
+    def __init__(self):
+        self.styles = {}
+        
+    def output(self, nesting=0):
+        if not self.styles:
+            return
+        
+        style_string = '<style>\n'
+      
+        for element, style in self.styles.items():
+            style_string += '  %s{\n' % element
+            
+            for key, value in style.items():
+                style_string += '    %s:\t%s;\n' % (key, value)
+            style_string += '  }\n'
+        
+        style_string += '  </style>\n\n'
+        
+        return style_string
--- examples.py
+++ examples.py
@@ -3,8 +3,19 @@ import PySVG
 my_svg = PySVG.SVG({'width':600})
 my_svg.attributes['height'] = 400
 
+# Adding child elements all at once
 my_svg.addChildElement('Text', {'id':'text-1', 'x':10, 'y':15}, 'This is a child')
-my_svg.addChildElement('rect', {'id':'rect-2', 'x':20, 'y':25, 'width':20, 'height':25, 'fill':'blue'})
+my_svg.addChildElement('rect', {'id':'rect-1', 'x':20, 'y':25, 'width':20, 'height':25})
 
+# Adding a child element piecemeal
+new_rect = PySVG.SVG_Element('rect', {'id':'rect-2', 'x':100, 'y':25})
+new_rect.attributes['height'] = 50
+new_rect.attributes['width'] = 80
+my_svg.children.append(new_rect)
+
+# Styling
+my_svg.addStyle('rect', ('fill', 'green'), ('stroke-width', 2), ('stroke', 'black'))
+
+# Write output to file
 fout = file('test.svg', 'w')
 fout.write(my_svg.output())
