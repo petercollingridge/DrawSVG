5cbfdae0b1672a3547aa7bc779d63da28e168d0c
Peter Collingridge <pcollingridge@pcolling-mac.oxfordnanolabs.local>
2019-01-23 13:14:32 +0000

    Sort data before sending to bar_chart function

--- quick_bar_charts.py
+++ quick_bar_charts.py
@@ -5,6 +5,7 @@ from math import floor, pow, log10
 def get_max_divisions(position_1, position_2):
     return max(2, floor(abs(position_2 - position_1) / 25))
 
+
 def getTickSize(start_value, end_value, max_divisions, min_unit=0):
     unit_value = max(min_unit, pow(10, floor(log10(end_value - start_value) - 1)))
 
@@ -55,7 +56,7 @@ def create_bar_chart(data, width=300, height=200):
     bar_labels = svg.add('g', { 'class': 'tick-labels tick-labels-x' })
 
     # Draw gridlines and y-axis labels
-    max_value = max(data.values())
+    max_value = max(item[1] for item in data)
     tick_size = getTickSize(0, max_value, 8)
     num_ticks = int(round(0.5 + max_value / tick_size))
     y_scale = lambda y: y2 - graph_height * y / max_value
@@ -78,10 +79,13 @@ def create_bar_chart(data, width=300, height=200):
     bar_x = x1 + (graph_width - bar_width * len(data)) / 2
     bar_width -= gap
 
-    for name, value in sorted(data.items(), key=lambda item: -item[1]):
+    for i, (name, value) in enumerate(data):
         y = round(y_scale(value))
         bars.add('rect', { 'x': bar_x, 'y': y, 'width': bar_width, 'height': y2 - y })
-        bar_labels.add('text', { 'x': bar_x + bar_width / 2, 'y': y2 + 6}, name)
+
+        if bar_width > 20 or i % 2:
+            bar_labels.add('text', { 'x': bar_x + bar_width / 2, 'y': y2 + 6}, name)
+
         bar_x += bar_width + gap
 
     # Draw axes
@@ -93,18 +97,24 @@ def create_bar_chart(data, width=300, height=200):
 
 if __name__ == '__main__':
     data = {
-        'the': 23014366,
-        'and': 11260177,
-        'of': 10968008,
-        'to': 10691399,
-        'a': 9392485,
-        'in': 7661696,
-        'that': 5416929,
-        'i': 4334454,
-        'it': 4062843,
-        'is': 3878929,
+        'the': 6.17,
+        'and': 3.02,
+        'of': 2.94,
+        'to': 2.87,
+        'a': 2.52,
+        'in': 2.05,
+        'that': 1.45,
+        'i': 1.16,
+        'it': 1.09,
+        'is': 1.04,
     }
+    # Sort by value
+    sorted_data = [item for item in sorted(data.items(), key=lambda item: -item[1])]
 
-    percent_data = { key: value * 100.0 / 372853319 for key, value in data.items() }
-    svg = create_bar_chart(percent_data)
+    data = {3: 78568162, 2: 64014624, 1: 13726939, 4: 67147137, 5: 42197602, 6: 30494576, 7: 27318266, 9: 12919188, 8: 19025650, 10: 8444737, 11: 4575234, 13: 1340132, 14: 499104, 12: 2404650, 15: 130590, 16: 33624, 17: 11537, 18: 1425, 19: 121, 20: 16, 21: 5}
+    data = { key: value * 100.0 / 372853319 for key, value in data.items() }
+    # Sort by key
+    sorted_data = [item for item in sorted(data.items(), key=lambda item: item[0])]
+
+    svg = create_bar_chart(sorted_data)
     svg.write('test.svg')
