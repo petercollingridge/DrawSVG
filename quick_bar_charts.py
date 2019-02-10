ce954a884f89a4fe73267d15bcce71a86b071b38
Peter Collingridge <peter.collingridge@gmail.com>
2019-02-10 21:51:55 +0000

    Import results and delete unneeded code.

--- quick_bar_charts.py
+++ quick_bar_charts.py
@@ -1,5 +1,7 @@
-import drawSVG
 from math import floor, pow, log10
+
+import drawSVG
+from results_to_graph import results
 
 
 def get_max_divisions(position_1, position_2):
@@ -26,7 +28,7 @@ def create_bar_chart(data, width=300, height=200, x_axis_label=None, y_axis_labe
         'viewBox': "0 0 {} {}".format(width, height)
     })
 
-    padding_top = 5
+    padding_top = 10
     padding_right = 1
     padding_left = 50
     padding_base = 40
@@ -101,104 +103,19 @@ def create_bar_chart(data, width=300, height=200, x_axis_label=None, y_axis_labe
 
 
 if __name__ == '__main__':
-    # Most common words
-    data = {
-        'the': 6.17,
-        'and': 3.02,
-        'of': 2.94,
-        'to': 2.87,
-        'a': 2.52,
-        'in': 2.05,
-        'that': 1.45,
-        'i': 1.16,
-        'it': 1.09,
-        'is': 1.04,
-    }
-
-    # data = { key: value * 100.0 / 404253213 for key, value in data.items() }
-    # data = { key: value * 100.0 / 372853319 for key, value in data.items() }
-
-    # Word lengths
-    data = {3: 78568162, 2: 64014624, 1: 13726939, 4: 67147137, 5: 42197602, 6: 30494576, 7: 27318266, 9: 12919188, 8: 19025650, 10: 8444737, 11: 4575234, 13: 1340132, 14: 499104, 12: 2404650, 15: 130590, 16: 33624, 17: 11537, 18: 1425, 19: 121, 20: 16, 21: 5}
-
-    # Letter frequencies
-    data = {
-        'e': 210488288,
-        't': 160074912,
-        'a': 136646210,
-        'o': 130580773,
-        'i': 123211411,
-        'n': 115604567,
-        's': 110356277,
-        'r': 100844356,
-        'h': 92255307,
-        'l': 68147506,
-        'd': 65774025,
-        'c': 49909466,
-        'u': 47431237,
-        'm': 40257103,
-        'f': 37153785,
-        'g': 36002208,
-        'w': 34532449,
-        'p': 34034177,
-        'y': 32337563,
-        'b': 25544945,
-        'v': 17133745,
-        'k': 13764819,
-        'x': 3049255,
-        'j': 2645573,
-        'q': 1443547,
-        'z': 1248190
-    }
-
-    total_letters = sum(data.values())
-    data = { key: value * 100.0 / total_letters for key, value in data.items() }
-
-    # Most common bigrams
-    data = {
-        'th': 49469612,
-        'he': 43638921,
-        'in': 33846509,
-        'er': 26689923,
-        'an': 25330844,
-        're': 24119390,
-        'at': 20442380,
-        'on': 20052624,
-        'nd': 17975233,
-        'en': 17859986,
-    }
-
-    data = { key: value * 100.0 / 1317618375 for key, value in data.items() }
+    data = results['ratio of starting to ending frequency']
 
     # Sort by value
     sorted_data = [item for item in sorted(data.items(), key=lambda item: -item[1])]
 
-    data = {3: 78568162, 2: 64014624, 1: 13726939, 4: 67147137, 5: 42197602, 6: 30494576, 7: 27318266, 9: 12919188, 8: 19025650, 10: 8444737, 11: 4575234, 13: 1340132, 14: 499104, 12: 2404650, 15: 130590, 16: 33624, 17: 11537, 18: 1425, 19: 121, 20: 16, 21: 5}
-    data = { key: value * 100.0 / 372853319 for key, value in data.items() }
-
-    data = {
-        'qu': 99.99896,
-        've': 68.50052,
-        'ze': 53.1492,
-        'he': 47.30234,
-        'ju': 43.10336,
-        'ke': 32.09455,
-        'jo': 31.17668,
-        'th': 30.90403,
-        'be': 29.74160,
-        'in': 27.47027,
-    }
-
     # Sort by key
     sorted_data = [item for item in sorted(data.items(), key=lambda item: -item[1])]
-    # Sort by key
-    # Sort by key
-    # sorted_data = [item for item in sorted(data.items(), key=lambda item: item[0])]
+
 
     svg = create_bar_chart(
         sorted_data,
         height=225,
-        x_axis_label="Bigrams",
+        x_axis_label="Letter",
         y_axis_label="Frequency (%)"
     )
 
