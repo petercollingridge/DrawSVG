89000b620fd9c32555e3447c2c554f4af5fab454
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-02-12 13:03:52 +0000

    Calculate the start and end points for axes

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
-from math import floor, log10
+from math import floor, ceil, log10
 
 
 def get_tick_size(start_value, end_value, max_divisions, min_unit=0):
     unit_value = max(min_unit, pow(10, floor(log10(end_value - start_value) - 1)))
+    max_value = abs(end_value)
 
     #  Increase unit value only using nice numbers
-    if unit_value < end_value / 20:
+    if unit_value < max_value / 20:
         unit_value *= 2
-    if unit_value < end_value / 16:
+    if unit_value < max_value / 16:
         unit_value *= 1.25
 
-    while unit_value < end_value / max_divisions:
+    while unit_value < max_value / max_divisions:
         unit_value *= 2
 
-    return unit_value
+    start_value = floor(start_value / unit_value) * unit_value
+    end_value = ceil(end_value / unit_value) * unit_value
+
+    return unit_value, start_value, end_value
 
 
 def get_max_divisions(position_1, position_2):
